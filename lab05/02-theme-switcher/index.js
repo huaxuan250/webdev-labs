19b587465e5fa72223d402a98b710189aa5bc9a0
huaxuan250 <huaxuan250@gmail.com>
2022-02-03 23:33:38 -0600

    Lab 05 Part 1 2 done

--- lab05/02-theme-switcher/index.js
+++ lab05/02-theme-switcher/index.js
@@ -5,3 +5,21 @@
     2. Modify the className property of the body tag 
        based on the button that was clicked.
 */
+
+const changeTheme = (theme) => {
+  document.querySelector("body").className = theme;
+};
+
+// We want to change the header color, body color, as well as font color
+document
+  .querySelector("#default")
+  .addEventListener("click", (e) => changeTheme("default"));
+document
+  .querySelector("#desert")
+  .addEventListener("click", (e) => changeTheme("desert"));
+document
+  .querySelector("#ocean")
+  .addEventListener("click", (e) => changeTheme("ocean"));
+document
+  .querySelector("#high-contrast")
+  .addEventListener("click", (e) => changeTheme("high-contrast"));
