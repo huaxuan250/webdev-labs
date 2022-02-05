3072f8b7b9b781662c1c59cc0f70bf646470c10b
huaxuan250 <huaxuan250@gmail.com>
2022-02-04 21:36:32 -0600

    Lab 05 Done

--- lab05/03-dyslexia-mode/index.js
+++ lab05/03-dyslexia-mode/index.js
@@ -2,3 +2,16 @@
   See Smashing Magazine Tutorial:
   https://www.smashingmagazine.com/2021/11/dyslexia-friendly-mode-website/
 */
+const changeTheme = () => {
+  const curr_theme = document.querySelector("body").className;
+  if (curr_theme === "dyslexia-mode") {
+    document.querySelector("body").className = "";
+  } else {
+    document.querySelector("body").className = "dyslexia-mode";
+  }
+};
+
+// We want to change the header color, body color, as well as font color
+document
+  .querySelector("#dyslexia-toggle")
+  .addEventListener("click", changeTheme);
